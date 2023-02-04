c2ada3a980dc1b8a56deea91dc2b7bff0cc8edda
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-02-04 22:58:23 +0200

    fix deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from 'pages/Home';
 import Library from 'pages/Library';
-import SharedLayout from './SharedLayout';
+// import SharedLayout from './SharedLayout';
 
 export const App = () => {
   return (
