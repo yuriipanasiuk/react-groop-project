00c5f9fa6afb147c94203e7c3ed4ae09bc6c7ddc
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-02-05 18:56:09 +0200

    delete shared layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from 'pages/Home';
 import Library from 'pages/Library';
-// import SharedLayout from './SharedLayout';
 
 export const App = () => {
   return (
@@ -10,11 +9,6 @@ export const App = () => {
         <Route index element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/library" element={<Library />} />
-        {/* <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Navigate to="home" />} />
-          <Route path="home" element={<Home />} />
-          <Route path="library" element={<Library />} />
-        </Route> */}
       </Routes>
     </div>
   );
